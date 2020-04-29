d9be436352c90424e5406f15f6ee960480996e7e
persianpros <persianpros@yahoo.com>
2020-04-29 10:46:24 +0430

    py files: E101 and E121 PEP8 cleanup
    Indentation contains mixed spaces and tabs (E101)
    Continuation line under-indented for hanging indent (E121)

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -3,9 +3,9 @@
 from Plugins.Plugin import PluginDescriptor
 
 def autostart(reason, **kwargs):
-	from Plugins.SystemPlugins.Servicelibpl import servicelibpl
+    from Plugins.SystemPlugins.Servicelibpl import servicelibpl
 
 def Plugins(**kwargs):
-	return [
-		PluginDescriptor(where = PluginDescriptor.WHERE_AUTOSTART, needsRestart = True, fnc = autostart)
-	]
+    return [
+            PluginDescriptor(where = PluginDescriptor.WHERE_AUTOSTART, needsRestart = True, fnc = autostart)
+    ]
