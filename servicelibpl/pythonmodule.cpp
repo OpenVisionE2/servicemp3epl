08712ad44dc434b9cc8dd51e0fbb7dd670ca7e4a
Hains <hainsvdbosch@ziggo.nl>
2020-05-03 10:05:03 +0200

    Map python2 c api functions
    Signed-off-by: persianpros <persianpros@gmail.com>

--- servicelibpl/pythonmodule.cpp
+++ servicelibpl/pythonmodule.cpp
@@ -6,8 +6,19 @@ static PyMethodDef servicelibplMethods[] =
 	{NULL,NULL,0,NULL}
 };
 
-PyMODINIT_FUNC
-initservicelibpl(void)
+static struct PyModuleDef moduledef = {
+	PyModuleDef_HEAD_INIT,
+	"servicelibpl",         /* m_name */
+	"servicelibpl",        /* m_doc */
+	-1,                  /* m_size */
+	servicelibplMethods,   /* m_methods */
+	NULL,                /* m_reload */
+	NULL,                /* m_traverse */
+	NULL,                /* m_clear */
+	NULL,                /* m_free */
+};
+
+PyMODINIT_FUNC PyInit_servicelibpl(void)
 {
-	Py_InitModule("servicelibpl", servicelibplMethods);
+	return PyModule_Create(&moduledef);
 }
